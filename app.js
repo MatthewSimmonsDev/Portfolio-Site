f931482464e82a96e8e3732585ffe1760cb72d5a
Matthew <matthewsimmonsdev@gmail.com>
2022-12-13 11:36:57 -0500

    trying to get images working

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.static('public'));
 app.set('view engine', 'pug');
 
 app.get("/", (req, res) =>{
-    res.render('index');
+    res.render('index', {data});
 });
 
 app.get("/about", (req, res) =>{
@@ -14,8 +14,29 @@ app.get("/about", (req, res) =>{
 });
 
 app.get("/project/:id", (req, res) =>{
-    res.render('project' + req.params.id)
+    const project = data.projects[req.params.id];
+    res.render('project', {project, data})
 });
+
+//error handler 404
+app.use((req, res, next) => {
+    console.log("404 error handler called")
+    const err = new Error();
+    err.status = 404;
+    res.status(err.status).send("Sorry that page doesn't exist.")
+  })
+
+//global error handler
+app.use((err, req, res, next) => {
+    if(err){console.log("Global error handler called", err)}
+    if(err.status === 404){
+        res.status(404).send(err)
+    } else {
+        err.message = err.message || `Oops! it looks like something went wrong.`;
+        res.status(err.status || 500).send(err);
+    }
+});
+
 
 app.listen(3000, function () {
     console.log("Server running on localhost 3000")
